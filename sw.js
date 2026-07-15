9c848fcd16f0f5f8c8b37522b2d982fcac8f455f
cleversondiasrosa2539-web <cleversondiasrosa2539@gmail.com>
2026-07-15 14:05:59 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'pilotando-meta-v1';
+const ASSETS = [
+  'index.html',
+  'manifest.json',
+  'https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;800&display=swap'
+];
+
+// Instala o Service Worker e guarda os arquivos no cache
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Ativa o Service Worker
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Serve os arquivos do cache quando estiver offline
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cachedResponse) => {
+      return cachedResponse || fetch(e.request);
+    })
+  );
+});
